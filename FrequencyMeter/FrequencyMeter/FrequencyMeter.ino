99aeee8c4421631f936f5c80077c9830b103ebf7
Alexandr <kobelchak@gmail.com>
2025-03-12 07:10:34 +0200

    create project

---
+++ FrequencyMeter/FrequencyMeter/FrequencyMeter.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		FrequencyMeter.ino
+ Created:	3/12/2025 7:08:24 AM
+ Author:	Alexandr
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
